0090721a7cf411136b031e17bd0815bb22a4e9d9
RobsonCassiano <robcassiano@gmail.com>
2026-03-27 22:16:42 -0300

    Aula json

--- app.js
+++ app.js
@@ -2,53 +2,27 @@
 
 import funcionarios from "./funcionarios.json" with { type: "json" }
 
-const criarContainer = () => {
-    const containerExistente = document.getElementById('container')
+function criarCard(funcionario) {
 
-    if (containerExistente) {
-        return containerExistente
-    }
+    const card = document.createElement("div")
+    card.className = 'card'
 
-    const novoContainer = document.createElement('div')
-    novoContainer.id = 'container'
-    document.body.appendChild(novoContainer)
+    const foto = document.createElement("img")
+    foto.src = `./img/${funcionario.imagem}`
+    foto.alt = `Foto de ${funcionario.nome}`
+    const nome = document.createElement("h3")
+    nome.textContent = funcionario.nome
+    const cargo = document.createElement("span")
 
-    return novoContainer
-}
+    cargo.textContent = funcionario.cargo
 
-const container = criarContainer()
-
-const criarCard = ({ nome, cargo, imagem }) => {
-    const card = document.createElement('div')
-    card.classList.add('card')
-
-    const foto = document.createElement('img')
-    foto.src = `./img/${imagem}`
-    foto.alt = `Foto de ${nome}`
-
-    const info = document.createElement('div')
-    info.classList.add('card-info')
-
-    const titulo = document.createElement('h3')
-    titulo.textContent = nome
-
-    const funcao = document.createElement('span')
-    funcao.textContent = cargo
-
-    info.append(titulo, funcao)
-    card.append(foto, info)
-
+    card.appendChild(foto, nome, cargo )
     return card
 }
 
-const carregarFuncionarios = () => {
-    container.replaceChildren()
+const cards= funcionarios.map(criarCard)
+document.getElementById("container").append(...cards)
 
-    funcionarios.forEach((funcionario) => {
-        const card = criarCard(funcionario)
-        container.appendChild(card)
-    })
-}
 
-carregarFuncionarios()
+
 
